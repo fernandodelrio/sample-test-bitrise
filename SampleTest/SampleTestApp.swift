47477b9a64b8f1253d94fff89383d5a7cb8f5662
Fernando del Rio <fernandomdr@gmail.com>
2021-11-29 17:57:28 -0300

    Adding stuff

---
+++ SampleTest/SampleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTestApp.swift
+//  SampleTest
+//
+//  Created by Fernando del Rio on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleTestTests/SampleTestTests.swift
@@ -0,0 +1,25 @@
+//
+//  SampleTestTests.swift
+//  SampleTestTests
+//
+//  Created by Fernando del Rio on 29/11/21.
+//
+
+import XCTest
+@testable import SampleTest
+
+class SampleTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
